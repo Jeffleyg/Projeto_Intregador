43e743bc36655ff6bfb0cc5c9f557111fac7393f
jeffleyg <jeffleygarcon007@gmail.com>
2024-05-15 00:55:50 -0300

    neste pull request eu implementei o setting o profile, as conexões entre as paginas

--- front_end/script.js
+++ front_end/script.js
@@ -99,3 +99,24 @@ function calculateTotal() {
 
     return total.toFixed(2); // Retorna o total como uma string formatada com duas casas decimais
 }
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    // Redireciona o usuário ou faz outras ações após o login bem-sucedido
+}
+
+function renderButton() {
+    gapi.signin2.render('googleSignIn', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSignIn,
+        'onfailure': onFailure
+    });
+}
